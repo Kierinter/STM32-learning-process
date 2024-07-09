186c220a88d78c98529a97fbf119a5161e4a7986
kierin <fsvuph2908@gmail.com>
2024-07-09 16:07:00 +0800

    fix hareware library

--- led_flow/HARDWARE/DMA/dma.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DMA_H
-#define __DMA_H
-
-#include"./sys/sys.h"
-
-void dma_enable(DMA_Stream_TypeDef *dma_streamx, uint16_t ndtr);                                                /* 使能一次DMA传输 */
-void dma_usart_tx_config(DMA_Stream_TypeDef *dma_streamx, uint8_t ch, uint32_t par, uint32_t mar);              /* 串口1 TX DMA初始化 */
-void dma_basic_config(DMA_Stream_TypeDef *dma_streamx,uint32_t ch, uint32_t par, uint32_t m0ar, uint32_t m1ar); /* DMA基本配置 */
-#endif
--- led_flow/HARDWARE/EXIT/exit.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EXIT_H
-#define __EXIT_H
-
-#include "./sys/sys.h"
-
-/* 引脚 和 中断编号 & 中断服务函数 定义 */ 
-
-#endif
