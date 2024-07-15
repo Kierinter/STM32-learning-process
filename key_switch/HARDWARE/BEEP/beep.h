4c6c4525adf778876563bfdba8da22253ab20fb4
kierin <hello48672@outlook.com>
2024-07-15 23:24:35 +0800

    add beep.c and beep.h

--- key_switch/HARDWARE/BEEP/beep.h
+++ key_switch/HARDWARE/BEEP/beep.h
@@ -3,7 +3,13 @@
 
 #include "sys.h"
 
-//引脚定义
+#define BEEP_GPIO_PORT                  GPIOF
+#define BEEP_GPIO_PIN                   SYS_GPIO_PIN8
+#define BEEP_GPIO_CLK_ENABLE()          do{ RCC->AHB1ENR |= 1 << 5; }while(0)
 
+#define BEEP(x)         sys_gpio_pin_set(BEEP_GPIO_PORT, BEEP_GPIO_PIN, x)      /* BEEP = x */
+#define BEEP_TOGGLE()   do{ BEEP_GPIO_PORT->ODR^=BEEP_GPIO_PIN; }while(0)       /* BEEP = !BEEP */
+
+void beep_init(void);    /* Initialize BEEP */
 
 #endif
